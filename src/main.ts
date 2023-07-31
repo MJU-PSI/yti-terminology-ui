91cad11d30a8094bc1dffee1fe7aa27c4d81db3e
Tadej Šivak <tadej.sivak@gora.si>
2023-07-31 15:09:13 +0200

    SoC fix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { APP_BASE_HREF } from '@angular/common';
 // Platform creation and bootstrapping of the application is delayed until we have loaded the configuration file.
 // The contents of the configuration file will be replaced (in Dockerfile) based on environment
 let configurationPath = `/configuration/configuration.json`;
-fetch(configurationPath)
+fetch(configurationPath, { method: 'GET', headers: {'Strict-Transport-Security': 'max-age=31536000; includeSubDomains'} })
   .then(response => response.json())
   .then((configuration: Configuration) => {
     if (configuration.production) {
