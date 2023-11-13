24a1831ca10519e3a8f1b24bd0c0676d9256029a
Aleš Bernik <ales.bernik@gora.si>
2023-11-13 11:32:59 +0100

    information about service

--- src/app/components/information/information-about-service.component.ts
+++ src/app/components/information/information-about-service.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LocationService } from '../../services/location.service';
+import { Subscription } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-information-about-service',
@@ -8,8 +10,20 @@ import { LocationService } from '../../services/location.service';
 })
 
 export class InformationAboutServiceComponent {
+  currentLang: String;
+  langChangeSubscription: Subscription;
 
-  constructor(private locationService: LocationService) {
+  constructor(private locationService: LocationService, private translateService: TranslateService) {
     locationService.atInformationAboutService();
+
+    this.currentLang = translateService.currentLang;
+
+    this.langChangeSubscription = translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.langChangeSubscription.unsubscribe();
   }
 }
